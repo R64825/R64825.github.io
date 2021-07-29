bd22b8f0577bc848309cc985abd002fb40afcb06
R64825 <R64825@gmail.com>
2021-07-29 23:25:43 +0800

    copy

---
+++ scripts/playlist.js
@@ -0,0 +1,29 @@
+trackList = [
+
+    {
+      name:"乙女解剖",
+      artist:"okayu",
+      src:"music/1.mp3"
+    },
+    {
+      name:"ヴァンパイア",
+      artist:"aqua",
+      src:"music/2.mp3"
+    },
+    {
+      name:"少女レイ",
+      artist:"suisei",
+      src:"music/3.mp3"
+    },
+    {
+      name:"flos",
+      artist:"okayu",
+      src:"music/4.mp3"
+    },
+    {
+      name:"少女レイ",
+      artist:"ayame",
+      src:"music/5.mp3"
+    },
+          
+];
